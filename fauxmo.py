5b3c5f0bafb974723dac002a9ee03fd2c6e9ba1d
Spence Konde <spencekonde@gmail.com>
2016-07-18 23:57:26 -0400

    more

--- fauxmo.py
+++ fauxmo.py
@@ -393,11 +393,11 @@ class fauxmo(upnp_device):
             if data.find('<BinaryState>1</BinaryState>') != -1:
                 # on
                 dbg("Responding to ON for %s" % self.name)
-                success = self.action_handler.on(client_address[0]))
+                success = self.action_handler.on(client_address[0])
             elif data.find('<BinaryState>0</BinaryState>') != -1:
                 # off
                 dbg("Responding to OFF for %s" % self.name)
-                success = self.action_handler.off(client_address[0]))
+                success = self.action_handler.off(client_address[0])
             else:
                 dbg("Unknown Binary State request:")
                 dbg(data)
